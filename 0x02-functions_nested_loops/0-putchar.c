7b3a40b6b654ed2ad5e7c77d0201002e3662315f
FGAnibaba <ajfavfables@gmail.com>
2023-02-21 14:28:52 -0800

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print -putchar
+ * code is written by Anibaba
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
