bbfb66f93b2d5167d05f6bfae4b8c7b33d813ecb
FGAnibaba <ajfavfables@gmail.com>
2023-02-27 13:51:24 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include 'main.h'
+#include <stdio.h>
+/**
+ * 0-reset_to_98.c
+ * @n: int to update
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void 0-reset_to_98.c;
+void 1-swap.c;
+void 2-strlen.c;
+void 3-puts.c;
+void 4-print_rev.c;
+void 5-rev_string.c;
+void 6-puts2.c;
+void 7-puts_half.c;
+void 8-print_array.c;
+void 9-strcpy.c;
+
+int -putchar(char c);
+
+#endif
