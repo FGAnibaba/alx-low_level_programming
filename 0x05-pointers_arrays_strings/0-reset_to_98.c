d0bd35d43ac8b3090020113e23db1c9def29dca1
FGAnibaba <ajfavfables@gmail.com>
2023-02-27 14:24:39 -0800

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * Return: 0
  */
 
-void reset_to_98(int *n) //function that takes a pointer to an int as parameter and updates the value it points to to 98.
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
