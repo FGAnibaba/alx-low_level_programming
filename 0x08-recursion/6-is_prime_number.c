ae8dd004d3280611a75e9437a7e3a7930833b6fb
FGAnibaba <ajfavfables@gmail.com>
2023-03-09 02:14:38 -0800

    task1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,14 +10,14 @@
 
 int is_prime(unsigned int n, unsigned int c)
 {
-if (n % c == 0)
-{
-if (n == c)
-return (1);
-else
-return (0);
-}
-return (0 + is_prime(n, c + 1));
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
 }
 
 /**
@@ -28,11 +28,11 @@ return (0 + is_prime(n, c + 1));
 
 int is_prime_number(int n)
 {
-if (n == 0)
-return (0);
-if (n < 0)
-return (0);
-if (n == 1)
-return (0);
-return (is_prime(n, 2));
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
