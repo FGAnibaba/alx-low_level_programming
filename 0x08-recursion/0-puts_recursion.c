5a2ca4b894d21fa253ef92831b88a8aabbd11ad1
FGAnibaba <ajfavfables@gmail.com>
2023-03-09 13:13:42 -0800

    task1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _put_recursion - prints a string followed by a new line.
+ *_put_recursion - prints a string followed by a new line.
  *@s: input
- *_puts_recursion(char *s)
- * Return: void.
+ *
+ * Return: 0
  */
 
 void _puts_recursion(char *s)
